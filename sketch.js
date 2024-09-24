98eba401218b199f561ce9f2a896a22730ab5909
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-09-24 00:19:39 -0400

    add star() function

--- sketch.js
+++ sketch.js
@@ -1,6 +1,31 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+function star(x, y, w, c) {
+  let r = w / 2;
+  push();
+  translate(x, y);
+  line(-c, -c, 0, -r);
+  line(0, -r, c, -c);
+
+  line(-c, c, 0, r);
+  line(0, r, c, c);
+
+  line(c, -c, r, 0);
+  line(r, 0, c, c);
+
+  line(-c, -c, -r, 0);
+  line(-r, 0, -c, c);
+  pop();
 }
 
-function draw() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(220, 120, 120);
+
+  strokeWeight(2);
+
+  star(80, 80, 150, 15);
+  star(200, 200, 100, 5);
+  star(430, 200, 200, 10);
+  star(250, 400, 60, 10);
 }
+
+function draw() {}
